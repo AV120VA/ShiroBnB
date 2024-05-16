14698a5f5996915b289ddeb2315c8f295b1e3934
Aaron Vargas <vargasaaron62@gmail.com>
2024-05-16 14:58:52 -0700

    added options and options.tableName

--- backend/db/migrations/20240516194001-create-spot-image.js
+++ backend/db/migrations/20240516194001-create-spot-image.js
@@ -6,45 +6,50 @@ if (process.env.NODE_ENV === "production") {
 }
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("SpotImages", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      spotId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Spot",
-          key: "id",
-          onUpdate: "CASCADE",
-          onDelete: "CASCADE",
+    await queryInterface.createTable(
+      "SpotImages",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        spotId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Spot",
+            key: "id",
+            onUpdate: "CASCADE",
+            onDelete: "CASCADE",
+          },
+        },
+        url: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        preview: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          defaultValue: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
         },
       },
-      url: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      preview: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("SpotImages");
+    options.tableName = "SpotImages";
+    await queryInterface.dropTable(options);
   },
 };
