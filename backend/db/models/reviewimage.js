b4d10387ee8a2b4bd541bc9ab979fffaac7b6d96
Aaron Vargas <vargasaaron62@gmail.com>
2024-05-16 09:38:42 -0700

    Added ReviewImages table

---
+++ backend/db/models/reviewimage.js
@@ -0,0 +1,43 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class ReviewImage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      ReviewImage.belongsTo(models.Review, {
+        foreignKey: "reviewId",
+      });
+    }
+  }
+  ReviewImage.init(
+    {
+      reviewId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Review",
+          key: "id",
+        },
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isURL: {
+            msg: "Invalid URL format",
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "ReviewImage",
+    }
+  );
+  return ReviewImage;
+};
