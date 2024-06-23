eac39564d5e0d7dd283fd8139d5b5049449e36a1
Aaron Vargas <vargasaaron62@gmail.com>
2024-06-23 00:11:27 -0700

    added correct database url

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,7 +9,8 @@ module.exports = {
     typeValidation: true,
   },
   production: {
-    use_env_variable: "https://api-project-cz3a.onrender.com",
+    use_env_variable:
+      "postgres://app_academy_projects_0hmn_user:wGCGbYH3rvz5kWtKdSQU9gZAR4AOfl1y@dpg-cp391jg21fec73b3ling-a/app_academy_projects_0hmn",
     dialect: "postgres",
     seederStorage: "sequelize",
     dialectOptions: {
