b0be0f020576fd33bca058b3806666d281b46928
Aaron Vargas <vargasaaron62@gmail.com>
2024-06-23 23:50:29 -0700

    wrote get all spots of current owner

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -10,4 +10,20 @@ router.get("/", async (req, res) => {
   res.status(200).json({ Spots: allSpots });
 });
 
+//get all spots owned by current user
+router.get("/current", async (req, res) => {
+  const userId = req.user.id;
+
+  let userSpots = await Spot.findAll({
+    where: { ownerId: userId },
+  });
+
+  res.status(200).json({ userSpots });
+});
+
+//get details of a spot by id
+router.get("/:spotId", async (req, res) => {
+  //need to make seeder files for spotImages in order to be able to test for the response body they want
+});
+
 module.exports = router;
