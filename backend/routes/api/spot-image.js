f5738ab3b3e13ae708eecb9100c5a82e82ae1277
Aaron Vargas <vargasaaron62@gmail.com>
2024-06-24 23:21:41 -0700

    adjusted logic error in delete spot image endpoint

--- backend/routes/api/spot-image.js
+++ backend/routes/api/spot-image.js
@@ -13,9 +13,9 @@ const router = express.Router();
 router.delete("/:imageId", requireAuth, async (req, res) => {
   let image = await SpotImage.findByPk(req.params.imageId);
   let spot = await Spot.findByPk(image.spotId);
-  let user = await User.findByPk(spot.ownerId);
+  let owner = await User.findByPk(spot.ownerId);
 
-  if (user.id !== spot.ownerId) {
+  if (owner.id !== req.user.id) {
     return res.status(403).json({
       message: "You are not authorized to perform this action",
     });
