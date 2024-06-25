258db963922cc20c6cf131971f42049fd25613dc
Aaron Vargas <vargasaaron62@gmail.com>
2024-06-24 23:17:41 -0700

    created spot-image router and dinished delete a spot image endpoint

---
+++ backend/routes/api/spot-image.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const { handleValidationErrors } = require("../../utils/validation");
+const { requireAuth } = require("../../utils/auth");
+
+const { Spot } = require("../../db/models");
+const { SpotImage } = require("../../db/models");
+const { User } = require("../../db/models");
+
+const router = express.Router();
+
+//delete a spot image
+router.delete("/:imageId", requireAuth, async (req, res) => {
+  let image = await SpotImage.findByPk(req.params.imageId);
+  let spot = await Spot.findByPk(image.spotId);
+  let user = await User.findByPk(spot.ownerId);
+
+  if (user.id !== spot.ownerId) {
+    return res.status(403).json({
+      message: "You are not authorized to perform this action",
+    });
+  }
+
+  if (!image) {
+    return res.status(404).json({
+      message: "Spot Image couldn't be found",
+    });
+  }
+
+  await image.destroy();
+
+  return res.status(200).json({
+    message: "Successfully deleted",
+  });
+});
+
+module.exports = router;
