bf2cea85c730f7055bfa85b2153e5ff838266447
Aaron Vargas <vargasaaron62@gmail.com>
2024-07-21 23:28:03 -0700

    added signup thunk

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -45,6 +45,23 @@ export const restoreUser = () => async (dispatch) => {
   return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const sessionReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_USER:
