14df229676e8634cb49939dda8d2d5fc3f1fe5b6
Aaron Vargas <vargasaaron62@gmail.com>
2024-12-13 22:26:06 -0800

    finished functionality and css for read reviews

--- frontend/src/component/Review/Review.jsx
+++ frontend/src/component/Review/Review.jsx
@@ -1,11 +1,15 @@
 import { useSelector } from "react-redux";
+import "./Review.css";
 
 function Review({ reviews }) {
   const sessionUser = useSelector((state) => state.session.user);
+
+  const sortedReviews = Object.values(reviews).sort((a, b) => b.id - a.id);
+
   return (
     <>
-      {Object.values(reviews).length > 0
-        ? Object.values(reviews).map((review) => {
+      {sortedReviews.length > 0
+        ? sortedReviews.map((review) => {
             let createdAt = new Date(review.createdAt);
             let formattedDate = createdAt.toLocaleString("default", {
               month: "long",
@@ -13,11 +17,13 @@ function Review({ reviews }) {
             });
             return (
               <div key={review.id} className="review-text">
-                <p className="review-user-first-name">
+                <h3 className="review-user-first-name">
                   {review.User.firstName}
+                </h3>
+                <p className="review-text bold review-date">{formattedDate}</p>
+                <p className="review-text review-description-text">
+                  {review.review}
                 </p>
-                <p className="review-date">{formattedDate}</p>
-                <p className="review-description-text">{review.review}</p>
                 {review.User.id === sessionUser.id ? (
                   <button
                     className="delete-review-button"
--- frontend/src/component/ReviewCallout/ReviewCallout.jsx
+++ frontend/src/component/ReviewCallout/ReviewCallout.jsx
@@ -9,29 +9,69 @@ function ReviewCallout({ spot }) {
   const sessionUser = useSelector((state) => state.session.user);
   const [isLoaded, setIsLoaded] = useState(false);
   const reviews = useSelector((state) => state.reviews.spotReviews);
+  const [alreadyReviewed, setAlreadyReviewed] = useState(false);
 
   useEffect(() => {
     dispatch(getReviewsById(spot.id)).then(() => setIsLoaded(true));
   }, [dispatch, spot.id]);
+
+  useEffect(() => {
+    if (isLoaded && reviews.length > 0) {
+      const reviewed = reviews.some(
+        (review) => review.User.id === sessionUser.id
+      );
+      setAlreadyReviewed(reviewed);
+    }
+  }, [isLoaded, reviews, sessionUser.id]);
 
   return (
     <>
       {isLoaded && (
         <div className="review-section-box">
           <div className="review-info-box">
-            <img className="review-section-star" src="/star.png" alt="star" />
+            <div className="star-and-rating">
+              <img className="review-section-star" src="/star.png" alt="star" />
+              {Object.values(reviews).length > 0 ? (
+                <>
+                  {" "}
+                  <p className="review-callout-text">
+                    {spot.avgStarRating.toFixed(1)}
+                  </p>
+                </>
+              ) : (
+                <p className="review-callout-text">0.0</p>
+              )}
+            </div>
             {Object.values(reviews).length > 0 ? (
-              <>
-                {" "}
-                <p>{spot.avgStarRating}</p>
-                <p># {spot.numReviews}</p>{" "}
-              </>
-            ) : (
-              <p>New!</p>
-            )}
+              <p className="review-callout-text">•</p>
+            ) : null}
+            <div className="review-count">
+              {Object.values(reviews).length > 0 ? (
+                <>
+                  {" "}
+                  <p className="review-callout-text">
+                    # {spot.numReviews}
+                  </p>{" "}
+                  {Object.values(reviews).length === 1 ? (
+                    <p className="review-callout-text">Review</p>
+                  ) : (
+                    <p className="review-callout-text">Reviews</p>
+                  )}
+                </>
+              ) : (
+                <p className="review-callout-text">New!</p>
+              )}
+            </div>
           </div>
-          {spot.ownerId !== sessionUser.id ? (
-            <button className="post-review-button">Post Your Review</button>
+          {spot.ownerId !== sessionUser.id &&
+          sessionUser &&
+          alreadyReviewed === true ? (
+            <>
+              <button className="post-review-button">Post Your Review</button>
+              {Object.values(reviews).length === 0 ? (
+                <p className="first-to-post">Be the first to post a review!</p>
+              ) : null}
+            </>
           ) : null}
           <Review reviews={reviews} />
         </div>
