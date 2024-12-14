9f2bb11a1d175032527eb5f6e450b16866f73e22
Aaron Vargas <vargasaaron62@gmail.com>
2024-12-13 20:48:28 -0800

    added text parts of reviews

--- backend/db/seeders/5-demo-reviews.js
+++ backend/db/seeders/5-demo-reviews.js
@@ -14,19 +14,37 @@ module.exports = {
       {
         spotId: 1,
         userId: 2,
-        review: "The place was very nice",
+        review: "The place was very nice. The vibe was immaculate.",
         stars: 5,
+      },
+      {
+        spotId: 1,
+        userId: 3,
+        review: "The place was very alright, could have been better.",
+        stars: 3,
       },
       {
         spotId: 2,
         userId: 3,
-        review: "The place was mid",
+        review: "The place was mid, but the host was nice!",
+        stars: 3,
+      },
+      {
+        spotId: 2,
+        userId: 1,
+        review: "The place was decent at best, should have stayed home.",
         stars: 3,
       },
       {
         spotId: 3,
         userId: 1,
-        review: "The place was horrible",
+        review: "The place was horrible, but the coffee shop nearby was nice.",
+        stars: 2,
+      },
+      {
+        spotId: 3,
+        userId: 2,
+        review: "The place was a disaster. It didnt feel safe to sleep in.",
         stars: 1,
       },
     ]);
--- frontend/src/component/Review/Review.jsx
+++ frontend/src/component/Review/Review.jsx
@@ -1,9 +1,19 @@
 function Review({ reviews }) {
-  console.log("HERE", reviews);
   return (
     <>
       {Object.values(reviews).map((review) => {
-        return <p>hi</p>;
+        let createdAt = new Date(review.createdAt);
+        let formattedDate = createdAt.toLocaleString("default", {
+          month: "long",
+          year: "numeric",
+        });
+        return (
+          <div key={review.id} className="review-text">
+            <p className="review-user-first-name">{review.User.firstName}</p>
+            <p className="review-date">{formattedDate}</p>
+            <p className="review-description-text">{review.review}</p>
+          </div>
+        );
       })}
     </>
   );
